2e46e414c74dd5aadad05c1877091506b5f2dffa
Your Name <you@example.com>
2020-04-20 10:31:30 -0500

    se entrena el modelo 1

--- entrenar_modelo.py
+++ entrenar_modelo.py
@@ -1,3 +1,3 @@
 def entrenar_modelo(modelo,x_entreno,y_entreno,x_pruebas,y_pruebas,callbacks):
     samples,din1,din2=x_entreno.shape
-    return modelo.fit(x=x_entreno,y=y_entreno,epochs=10,validation_data=(x_pruebas,y_pruebas),batch_size=samples,callbacks=callbacks)
+    return modelo.fit(x=x_entreno,y=y_entreno,epochs=1000,validation_data=(x_pruebas,y_pruebas),batch_size=samples,callbacks=callbacks)
--- main_prediccion_lstm.py
+++ main_prediccion_lstm.py
@@ -29,7 +29,7 @@ print("shape of x is"+str(y_entreno.shape))
 print("shape of x is"+str(x_pruebas.shape))
 print("shape of x is"+str(y_pruebas.shape))
 modelo_lstm=generar_modelo_lstm.generar_modelo_lstm(x_entreno)
-callback_guardar=callback_guardar.callback_guardar("modelos/modelo1/epoch50")
+callback_guardar=callback_guardar.callback_guardar("modelos/modelo1/epoch1000")
 #cargar un modelo guardado
 #checkpoint_path_charge = ""
 #modelo_lstm.load_weights(checkpoint_path_charge)
