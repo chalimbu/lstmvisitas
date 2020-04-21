6b0177d4ae74c44dbad30157a6be540f2f6a3bba
Your Name <you@example.com>
2020-04-21 18:40:24 -0500

    se desarrolla/entrena el modelo 4

--- generar_modelo_lstm.py
+++ generar_modelo_lstm.py
@@ -6,9 +6,8 @@ import tensorflow as tf
 
 def generar_modelo_lstm(x_data):
     model = Sequential([
-                       LSTM(60,dropout=0.2,input_shape=(30,1),return_sequences=True),
-                       LSTM(60,dropout=0.5,return_sequences=True),
-                       LSTM(60, dropout=0.5),
+                       LSTM(200,dropout=0.2,input_shape=(30,1),return_sequences=True),
+                       LSTM(200,dropout=0.5),
                        Dense(1)])
     opt = tf.keras.optimizers.Adam()
     mse = tf.keras.losses.MeanSquaredError()
--- main_prediccion_lstm.py
+++ main_prediccion_lstm.py
@@ -29,7 +29,7 @@ print("shape of x is"+str(y_entreno.shape))
 print("shape of x is"+str(x_pruebas.shape))
 print("shape of x is"+str(y_pruebas.shape))
 modelo_lstm=generar_modelo_lstm.generar_modelo_lstm(x_entreno)
-callback_guardar=callback_guardar.callback_guardar("modelos/modelo3/epoch1000")
+callback_guardar=callback_guardar.callback_guardar("modelos/modelo4/epoch1000")
 #cargar un modelo guardado
 #checkpoint_path_charge = "modelos/modelo2/epoch1000"
 #modelo_lstm.load_weights(checkpoint_path_charge)
